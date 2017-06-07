031e5873862579af77a022e4e6c0d826cae79f0a
Yoann Gini <yoann.gini@gmail.com>
2017-06-07 23:48:31 +0200

    Adding framework to workspace

---
+++ EasyLogin/EasyLogin/EasyLogin.h
@@ -0,0 +1,19 @@
+//
+//  EasyLogin.h
+//  EasyLogin
+//
+//  Created by Yoann Gini on 07/06/2017.
+//  Copyright © 2017 EasyLogin. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for EasyLogin.
+FOUNDATION_EXPORT double EasyLoginVersionNumber;
+
+//! Project version string for EasyLogin.
+FOUNDATION_EXPORT const unsigned char EasyLoginVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EasyLogin/PublicHeader.h>
+
+
