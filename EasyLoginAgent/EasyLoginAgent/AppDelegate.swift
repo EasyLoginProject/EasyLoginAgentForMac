8a5a969c62057df572b03b599bccf02504afd40d
Yoann Gini <yoann.gini@gmail.com>
2017-06-08 09:23:29 +0200

    Test implementation for Agent <—> XPC Service exchange

--- EasyLoginAgent/EasyLoginAgent/AppDelegate.swift
+++ EasyLoginAgent/EasyLoginAgent/AppDelegate.swift
@@ -7,20 +7,29 @@
 //
 
 import Cocoa
+import EasyLogin
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
-
-
+    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        EasyLoginDBProxy.sharedInstance().getAllRegisteredRecords(ofType: "user", withAttributesToReturn: ["shortname"]) { (records, error) in
+            
+            print(records as Any)
+            
+            EasyLoginDBProxy.sharedInstance().registerRecord(["shortname": "test", "displayname": "Test 1"], ofType: "user", withUUID: UUID.init().uuidString)
+            
+            EasyLoginDBProxy.sharedInstance().getAllRegisteredRecords(ofType: "user", withAttributesToReturn: ["shortname"]) { (records, error) in
+                
+                print(records as Any)
+            }
+        }
     }
-
+    
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
-
-
+    
+    
 }
 
