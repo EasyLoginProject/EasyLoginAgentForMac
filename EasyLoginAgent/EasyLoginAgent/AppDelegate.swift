b68345d6d300c22ccfcf910ee50e3b792186dc8e
Yoann Gini <yoann.gini@gmail.com>
2017-07-02 11:55:21 +0200

    Replacing print by NSLog to take avantage of time prefix.

--- EasyLoginAgent/EasyLoginAgent/AppDelegate.swift
+++ EasyLoginAgent/EasyLoginAgent/AppDelegate.swift
@@ -20,14 +20,14 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         
         ELCachingDBProxy.sharedInstance().testXPCConnection { (error) in
             if let error = error {
-                print("EasyLoginAgent - XPC test done with error: \(error)")
+                NSLog("EasyLoginAgent - XPC test done with error: \(error)")
             } else {
-                print("EasyLoginAgent - XPC test done with success")
+                NSLog("EasyLoginAgent - XPC test done with success")
             }
         }
         
         notificationObject = NotificationCenter.default.addObserver(forName:Notification.Name(kELServerUpdateNotification), object:nil, queue:nil, using: { (notification) in
-            print("EasyLoginAgent - Server has changed, we start the sync")
+            NSLog("EasyLoginAgent - Server has changed, we start the sync")
             self.syncRegisteredUsers()
         })
         
@@ -38,17 +38,17 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     }
     
     func registerMyDevice(completionHandler: @escaping ((ELDevice?) -> ())) {
-        print("EasyLoginAgent - Looking for existing device records")
+        NSLog("EasyLoginAgent - Looking for existing device records")
         server?.getAllRecords(withEntityClass: ELDevice.recordClass(), completionBlock: { (records, error) in
             var maybeMyself: ELDevice?
             
             if let records = records {
-                print("EasyLoginAgent - Found multiple records, looking for the one matching our serial number")
+                NSLog("EasyLoginAgent - Found multiple records, looking for the one matching our serial number")
                 for record in records  {
                     let device = record as! ELDevice
                     
                     if device.string(forProperty: kELDeviceSerialNumberKey) == ELToolbox.serialNumber() {
-                        print("EasyLoginAgent - Record with our serial number found")
+                        NSLog("EasyLoginAgent - Record with our serial number found")
                         maybeMyself = device
                         break
                     }
@@ -56,29 +56,29 @@ class AppDelegate: NSObject, NSApplicationDelegate {
             }
             
             if let myself = maybeMyself {
-                print("EasyLoginAgent - Device record found")
+                NSLog("EasyLoginAgent - Device record found")
                 completionHandler(myself)
             } else {
-                print("EasyLoginAgent - Device record not found, listing our own invetory properties")
+                NSLog("EasyLoginAgent - Device record not found, listing our own invetory properties")
                 let myProperties = ELRecordProperties(dictionary: ["deviceName" : Host.current().localizedName!,
                                                                    "serialNumber" : ELToolbox.serialNumber()],
                                                       mapping: nil)
                 
                 if let myProperties = myProperties {
-                    print("EasyLoginAgent - Inventory properties ready, sending request to create our own record")
+                    NSLog("EasyLoginAgent - Inventory properties ready, sending request to create our own record")
                     self.server?.createNewRecord(withEntityClass: ELDevice.recordClass(),
                                             properties: myProperties,
                                             completionBlock: { (record, error) in
                                                 if let record = record {
-                                                    print("EasyLoginAgent - Record created with success")
+                                                    NSLog("EasyLoginAgent - Record created with success")
                                                     completionHandler(record as? ELDevice)
                                                 } else {
-                                                    print("EasyLoginAgent - Unable to create record, error \(String(describing: error))")
+                                                    NSLog("EasyLoginAgent - Unable to create record, error \(String(describing: error))")
                                                     completionHandler(nil)
                                                 }
                     })
                 } else {
-                    print("EasyLoginAgent - Error! Unable to list our own inventory properties")
+                    NSLog("EasyLoginAgent - Error! Unable to list our own inventory properties")
                     completionHandler(nil)
                 }
             }
@@ -93,21 +93,21 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     }
     
     func syncRegisteredUsers() {
-        print("EasyLoginAgent - Fetch all assigned users for registration and update")
+        NSLog("EasyLoginAgent - Fetch all assigned users for registration and update")
         
         if let myRecord = myRecord {
             server?.getUpdatedRecord(myRecord, completionBlock: { (updatedMyself, error) in
                 if let updatedMyself = updatedMyself {
                     let myself = updatedMyself as! ELDevice
                     
-                    print("EasyLoginAgent - Sync based on info \(myself)")
+                    NSLog("EasyLoginAgent - Sync based on info \(myself)")
                 } else {
-                    print("EasyLoginAgent - Unable to fetch our own record, no sync possible")
+                    NSLog("EasyLoginAgent - Unable to fetch our own record, no sync possible")
                 }
             })
 
         } else {
-            print("EasyLoginAgent - Unable to fetch our own record, no sync possible")
+            NSLog("EasyLoginAgent - Unable to fetch our own record, no sync possible")
             DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(10), execute: {
                 self.syncRegisteredUsers()
             })
@@ -121,32 +121,32 @@ class AppDelegate: NSObject, NSApplicationDelegate {
                 
                 for record in records {
                     self.server?.getUpdatedRecord(record, completionBlock: { (updatedRecord, error) in
-                        print("EasyLoginAgent - Register record \(String(describing: updatedRecord))")
+                        NSLog("EasyLoginAgent - Register record \(String(describing: updatedRecord))")
                         ELCachingDBProxy.sharedInstance().registerRecord(updatedRecord?.dictionaryRepresentation(), ofType:updatedRecord?.recordEntity(), withUUID:updatedRecord?.identifier())
                     })
                     
                     wantedUUIDs.insert(record.identifier())
                 }
                 
-                print("EasyLoginAgent - Fetch all registered users for cleanup")
+                NSLog("EasyLoginAgent - Fetch all registered users for cleanup")
                 ELCachingDBProxy.sharedInstance().getAllRegisteredUUIDs(ofType:ELUser.recordEntity(), andCompletionHandler: { (registeredUUIDs, error) in
                     
                     if let registeredUUIDs = registeredUUIDs {
-                        print("EasyLoginAgent - Registered UUIDs before cleanup: \(registeredUUIDs)")
+                        NSLog("EasyLoginAgent - Registered UUIDs before cleanup: \(registeredUUIDs)")
                         let existingUUIDs = Set(registeredUUIDs)
                         let unwantedUUIDs = existingUUIDs.subtracting(wantedUUIDs)
                         
                         for unwantedUUID in unwantedUUIDs {
-                            print("EasyLoginAgent - Unregister user with UUID \(unwantedUUID)")
+                            NSLog("EasyLoginAgent - Unregister user with UUID \(unwantedUUID)")
                             ELCachingDBProxy.sharedInstance().unregisterRecord(ofType:ELUser.recordEntity(), withUUID: unwantedUUID)
                         }
                     } else {
-                        print("EasyLoginAgent - No registered UUIDs found during the cleanup step.")
+                        NSLog("EasyLoginAgent - No registered UUIDs found during the cleanup step.")
                     }
-                    print("EasyLoginAgent - Cleanup done")
+                    NSLog("EasyLoginAgent - Cleanup done")
                 })
                 
-                print("EasyLoginAgent - Sync done")
+                NSLog("EasyLoginAgent - Sync done")
                 
             }
         })
